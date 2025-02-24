1acc2ce976e5e6c4df619a3430bd31e1e369d405
Jaep <jaep0928@gmail.com>
2025-02-24 10:18:17 -0600

    Primer Grafico Canvas

---
+++ JS/main.js
@@ -0,0 +1,7 @@
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+ctx.fillStyle = "green";
+ctx.fillRect(10, 10, 100, 100);
+
+// Rellenar el canvas completo
+// ctx.fillRect(0, 0, canvas.width, canvas.height);
